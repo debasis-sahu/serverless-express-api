ca067ba526a07fca243b7167fc13c6b9a43ab045
Debasis Sahu (Home) <debasis.sahu@quantta.com>
2025-05-22 00:52:15 +0530

    Update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const http=require('http');
-const app=require('./app');
+const http = require('http');
+const app = require('./routes');
 
-const server=http.createServer(app);
+const server = http.createServer(app);
 
-const port=process.env.PORT || 80;
-server.listen(port,()=>{
-    console.log('Server is running on port:'+port);
+const port = process.env.PORT || 80;
+server.listen(port, () => {
+  console.log('Server is running on port:' + port);
 });
